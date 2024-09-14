2ba212417ce7ed2c6bd54bf770bfaf5adc73cbf8
tpgus2603 <kakaneymar2424@gmail.com>
2024-09-14 13:18:03 +0900

    first

---
+++ src/main/java/hello/netronics/NetronicsApplication.java
@@ -0,0 +1,13 @@
+package hello.netronics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetronicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetronicsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/netronics/NetronicsApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.netronics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetronicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
